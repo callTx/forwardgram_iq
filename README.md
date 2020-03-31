ae01ad3225b46fef6fddb4a0fc16d9908b6e8120
Daniel <danielfdeus@gmail.com>
2020-03-31 11:18:48 -0300

    fix UnicodeEncodeError: 'charmap' codec can't encode characters error

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# forwardgram_iq
-Forward messages from telegram channels to another
+# forwardgram
+Forward messages from multiple Telegram channels to one of your own!
+
+## Setup
+- `python3.7 -m pip install -r requirements.txt`.
+- Fill out a configuration file. An exmaple file can be found at `config.yml-sample`. 
+
+## Run
+`python3.7 forwardgram.py config.yml`
---
+++ forwardgram.py
@@ -0,0 +1,51 @@
+from telethon import TelegramClient, events, sync
+from telethon.tl.types import InputChannel
+import yaml
+import sys
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('telethon').setLevel(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+def start(config):
+    client = TelegramClient(config["session_name"], 
+                            config["api_id"], 
+                            config["api_hash"])
+    client.start()
+
+    input_channels_entities = []
+    output_channel_entity = None
+    for d in client.iter_dialogs():
+        if d.name in config["input_channel_names"]:
+            input_channels_entities.append(InputChannel(d.entity.id, d.entity.access_hash))
+        if d.name == config["output_channel_name"]:
+            output_channel_entity = InputChannel(d.entity.id, d.entity.access_hash)
+            
+    if output_channel_entity is None:
+        logger.error(f"Could not find the channel \"{config['output_channel_name']}\" in the user's dialogs")
+        sys.exit(1)
+    logging.info(f"Listening on {len(input_channels_entities)} channels. Forwarding messages to {config['output_channel_name']}.")
+    
+    @client.on(events.NewMessage(chats=input_channels_entities))
+    async def handler(event):
+        await client.forward_messages(output_channel_entity, event.message)
+        logging.info(f"Event message: {event.message.message}.")
+
+        with open('A:1.txt', 'w',encoding="utf-8") as filehandle:
+            filehandle.write(event.message.message)
+            filehandle.close()
+
+
+
+    client.run_until_disconnected()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} CONFIG_PATH")
+        sys.exit(1)
+    with open(sys.argv[1], 'rb') as f:
+        config = yaml.load(f)
+    start(config)
